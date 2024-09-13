180f9c0d568c52a8be04709b6cf77e44fe03d2ca
hisham2630 <35382590+hisham2630@users.noreply.github.com>
2024-09-13 21:28:46 +0300

    Update build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -536,14 +536,14 @@ fi
 #   }
 #    clear
     
-else
-echo "Circumventing crappy SELinux's limitations... (Thanks BrunoSX)"
-patch -d wine -Np1 < "${scriptdir}"/ndis-proot.patch || {
-        echo "Error: Failed to apply one or more patches."
-        exit 1
-    }
-    clear
-fi
+#else
+#echo "Circumventing crappy SELinux's limitations... (Thanks BrunoSX)"
+#patch -d wine -Np1 < "${scriptdir}"/ndis-proot.patch || {
+#        echo "Error: Failed to apply one or more patches."
+#        exit 1
+#    }
+#    clear
+#fi
 
 #echo "Adding virtual memory environment variable (fixes some games) (credits to BrunoSX for the initial idea)"
 #patch -d wine -Np1 < "${scriptdir}"/virtualmemory.patch || {
